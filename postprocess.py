767c8ee7a069e4edcbfb6488a908e06b38451af9
Arpan Tripathi <tripathiarpan20@gmail.com>
2023-03-23 11:38:08 +0900

    added bg removal

--- postprocess.py
+++ postprocess.py
@@ -56,161 +56,157 @@ def cut(img: Image, format = 'PNG'):
                 
     return img
 
-#------------------ summerstay's strongest component masking algo -------------------------------
 
-def convertPILtocv2(im):
-    cv2_im = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
-    return cv2_im
+#-----------------
+# https://github.com/webaverse-studios/spritesheet-api
+#-----------------
 
-# def convertcv2toPIL(cv2_im):
-#     pil_im = Image.fromarray(cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB))
-#     return pil_im
+def getLeftOrRight(img: Image):
+    img = img.convert('RGBA')
 
-def convertcv2toPIL(cv2_im):
-    (height, width, colors) = cv2_im.shape
-    if colors == 4:
-        pil_im = Image.fromarray(cv2.cvtColor(cv2_im, cv2.COLOR_BGRA2RGBA))
-    else:
-        pil_im = Image.fromarray(cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB))
-    return pil_im
+    width, height = img.size
+    pixels = img.getcolors(width * height)
+    most_frequent_pixel = pixels[0]
 
-def mask_from_black(generated_image, init_image, outer_tolerance=35, inner_tolerance=7, radius = 70):
-    pic = convertPILtocv2(generated_image)
-    init_pic = convertPILtocv2(init_image)
-    grey_mask = cv2.cvtColor(init_pic, cv2.COLOR_BGR2GRAY)
-    ret, starting_mask = cv2.threshold(grey_mask, 0, 255, cv2.THRESH_BINARY_INV)
-    (height, width, colors) = pic.shape
-    gray_img = cv2.cvtColor(pic , cv2.COLOR_BGR2GRAY)
-    threshold = cv2.threshold(gray_img, inner_tolerance, 255, cv2.THRESH_BINARY)[1]
-    analysis = cv2.connectedComponentsWithStats(threshold,4,cv2.CV_32S)
-    (totalLabels, label_ids, values, centroid) = analysis
-    output = np.zeros(gray_img.shape, dtype="uint8")
-    biggest_area = 0
-    biggest_index = 0
-    for i in range(1, totalLabels):
-      # Area of the component
-        area = values[i, cv2.CC_STAT_AREA]
-        if area>biggest_area:
-            biggest_area = area
-            biggest_index = i
-        
-    componentMask = (label_ids == biggest_index).astype("uint8") * 255
-    pic = cv2.bitwise_and(pic, pic, mask=componentMask)
+    for count, colour in pixels:
+        if count > most_frequent_pixel[0]:
+            most_frequent_pixel = (count, colour)
 
-    # place a tiny black square in each corner to remove any stray pixels
-    pic[0:3,0:3,:]=(0,0,0)
-    pic[height-3:height,0:3,:]=(0,0,0)
-    pic[0:3,width-3:width,:]=(0,0,0)
-    pic[height-3:height,width-3:width,:]=(0,0,0)
+    for x in range(width):
+        for y in range(height):
+            pixel = img.getpixel((x, y))
+            if abs(pixel[0] - most_frequent_pixel[1][0]) < 10 and abs(pixel[1] - most_frequent_pixel[1][1]) < 10 and abs(pixel[2] - most_frequent_pixel[1][2]) < 10:
+                img.putpixel((x, y), (255, 255, 255, 0))
+    
+    
+    img = img.convert('RGBA')
+    width, height = img.size
+    cel_width = width / 4
+    cel_height = height / 4
+    cell = img.crop((0, 0, cel_width, cel_height))
+    pixel = cell.getpixel((cel_width / 2, cel_height / 2))
+    alpha = pixel[3]
+    if alpha == 0:
+       return False #right
+    else: 
+        return True #left
+    
 
-    #floodfill from the outside corners so that everything approximately black that is connected to the outside corners becomes completely black.
-    ot = outer_tolerance
-    cv2.floodFill(pic, None, (2,width-2), (0,0,0), (ot, ot, ot, ot), (ot, ot, ot, ot), cv2.FLOODFILL_FIXED_RANGE) 
-    cv2.floodFill(pic, None, (height-2,2), (0,0,0), (ot, ot, ot, ot), (ot, ot, ot, ot), cv2.FLOODFILL_FIXED_RANGE) 
-    cv2.floodFill(pic, None, (2,2), (0,0,0), (ot, ot, ot, ot), (ot, ot, ot, ot), cv2.FLOODFILL_FIXED_RANGE) 
-    cv2.floodFill(pic, None, (height-2,width-2), (0,0,0), (ot, ot, ot, ot), (ot, ot, ot, ot), cv2.FLOODFILL_FIXED_RANGE)
+def postprocessImg(img1: Image):
+    isLeft = getLeftOrRight(img1)
+    if not isLeft:
+        width = img1.size[0]
+        height = img1.size[1]
+        qHeight = height / 4
 
-    #make everything anywhere in the image that is nearly black completely black. This is usually done at a lower tolerance than the outer tolerance.
-    lower = np.array([0, 0, 0], dtype="uint8")
-    upper = np.array([inner_tolerance, inner_tolerance, inner_tolerance], dtype="uint8")
-    cv2mask = cv2.inRange(pic, lower, upper)
-    #keep the center of the original mask and only affect the edges
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (radius, radius))
-    eroded = cv2.erode(starting_mask,kernel)
-    cv2mask = cv2.bitwise_or(cv2mask,eroded)
-    dilated =cv2.dilate(starting_mask, kernel)
-    cv2mask = cv2.bitwise_and(cv2mask,dilated)
-    mask = convertcv2toPIL(cv2mask)
-    # cv2.imshow("mask",cv2mask)
-    # cv2.waitKey(0)
-    return mask
+        def cutImgTo4Rows(img: Image):
+            imgs = []
+            for i in range(4):
+                imgs.append(img.crop((0, i * qHeight, width, (i + 1) * qHeight)))
+            return imgs
 
-def cut_magenta(input_pil, outer_tolerance = 80):
-    # input_image = cv2.imread(input_filename)
-    input_image = convertPILtocv2(input_pil)
+        imgs1 = cutImgTo4Rows(img1)
 
-    (height, width, colors) = input_image.shape
-    # convert the magenta background image into a black background image by inverting the red and blue channels.
-    input_image[:,:,0] = 255-input_image[:,:,0]
-    input_image[:,:,2] = 255-input_image[:,:,2]
-    # the opacity is a clipped linear function of the maximum of the green channel and the inverted red and vlue channels.
-    opacity = np.clip(2.0*np.amax(np.double(input_image),2)-64,0,255)
-    transparent_array = np.zeros((input_image.shape[0],input_image.shape[1],4),np.uint8)
-    transparent_array[:,:,:3]=input_image
-    transparent_array[:,:,3]=opacity
-    im = transparent_array
-    # now reinvert the colors so that they are back to normal
-    im[:,:,2] = 255 - im[:,:,2]
-    im[:,:,0] = 255 - im[:,:,0]
-    # next make a new transparency mask that is just for the surrounding backhground using floodfill
-    ot = outer_tolerance
-    outer_mask = np.zeros((input_image.shape[0]+2,input_image.shape[1]+2,1),np.uint8)
-    for x in range(2,width-2,20):
-        for y in [2,height-2]:
-            if input_image[y,x,0]>0: 
-                cv2.floodFill(input_image, outer_mask, (x,y), (0,0,0), (ot, ot, ot, ot), (ot, ot, ot, ot), cv2.FLOODFILL_FIXED_RANGE)
-    for y in range(2,height-2,20):
-        for x in [2,width-2]:
-            if input_image[y,x,0]>0: 
-                cv2.floodFill(input_image, outer_mask, (x,y), (0,0,0), (ot, ot, ot, ot), (ot, ot, ot, ot), cv2.FLOODFILL_FIXED_RANGE)  
-    outer_mask = 255*(1-outer_mask)
-    #take the minimum brightness (bitwise and) of the two methods to get the final transparency.
-    im[:,:,3] = cv2.bitwise_and(im[:,:,3],outer_mask[1:-1,1:-1])
-    #finally resize it to 128x128 and then upscale it again to make it pixelated.
-    small=cv2.resize(im,(128,128))
-    small=cv2.resize(small,(512,512),interpolation=cv2.INTER_NEAREST)
-    #at this point you could convert it to a PIL image, but I haven't really tested that.
-    # cv2.imwrite(input_filename + "_small.png", small)
-    return convertcv2toPIL(small)
+        imgs1[1], imgs1[2] = imgs1[2], imgs1[1]
+
+        img1 = Image.new("RGBA", (width, height))
+        for i in range(4):
+            img1.paste(imgs1[i], (0, int(i * qHeight)))
+
+        return img1
+    return img1
+
+def crop(img):
+    img = img.convert('RGBA')
+    width, height = img.size
+    cel_width = width / 4
+    cel_height = height / 4
+    cell = img.crop((0, 0, cel_width, cel_height))
+    pixel = cell.getpixel((cel_width / 2, cel_height / 2))
+    alpha = pixel[3]
+    if alpha == 0:
+        img = img.crop((cel_width, 0, width, height))
+    else: 
+        img = img.crop((0, 0, width - cel_width, height))
+    return img
+
+def cleanImage(img: Image):
+   # Convert the image to RGBA format
+    img = img.convert('RGB')
+    im_array = np.array(img)
+
+    # Get the shape of the image
+    height, width, channels = im_array.shape
+
+    # Set the color thresholds for the background colors
+    color_threshold_r = 10
+    color_threshold_g = 10
+    color_threshold_b = 10
+
+    # Loop through the pixels in the image
+    for y in range(height):
+        for x in range(width):
+            # Get the RGB values of the pixel
+            r, g, b = im_array[y, x]
+
+            # Check if the pixel is a shade of green
+            if abs(r - 0) < color_threshold_r and abs(b - 0) < color_threshold_b:
+                # Check the surrounding pixels to see if the current pixel is surrounded by pixels with the same color
+                surrounding_pixels = [(y-1, x-1), (y-1, x), (y-1, x+1), (y, x-1), (y, x+1), (y+1, x-1), (y+1, x), (y+1, x+1)]
+                same_color = True
+                for pixel in surrounding_pixels:
+                    y_pos, x_pos = pixel
+                    if y_pos < 0 or y_pos >= height or x_pos < 0 or x_pos >= width:
+                        continue
+                    r_surrounding, g_surrounding, b_surrounding = im_array[y_pos, x_pos]
+                    if abs(r - r_surrounding) > color_threshold_r or abs(g - g_surrounding) > color_threshold_g or abs(b - b_surrounding) > color_threshold_b:
+                        same_color = False
+                        break
+                if same_color:
+                    # Set the alpha value of the pixel to 0 to make it transparent
+                    im_array[y, x] = 0
+
+    # Create a new image from the modified NumPy array
+    transparent_im = Image.fromarray(im_array)
+    
+      # Create a new image from the modified NumPy array
+    transparent_im = Image.fromarray(im_array)
+
+    #remove white background
+    transparent_im = transparent_im.convert("RGBA")
+    datas = transparent_im.getdata()
+
+    newData = []
+    for item in datas:
+        if item[0] == 255 and item[1] == 255 and item[2] == 255:
+            newData.append((255, 255, 255, 0))
+        else:
+            newData.append(item)
+
+    transparent_im.putdata(newData)
+
+    # Save the transparent image
+    return transparent_im
 
 
-def cutv2(generated_image:Image, init_image:Image, format = 'PNG', outer_tolerance=35, inner_tolerance=7, radius = 70):
-  mask = mask_from_black(generated_image, init_image, outer_tolerance=outer_tolerance, inner_tolerance=inner_tolerance, radius = radius)
-  generated_image = generated_image.convert('RGBA')
-  img_arr = np.array(generated_image)
-  img_arr[:,:,3] = 255 - np.array(mask.convert('L'))
-  return Image.fromarray(img_arr, mode = 'RGBA')
+def cleanImage_wrapper(img):
+  img = postprocessImg(img)
+  img = img.convert('RGBA')
 
-#------------Magenta ControlNet Canny Pixel model----------------
+  width, height = img.size
+  pixels = img.getcolors(width * height)
+  most_frequent_pixel = pixels[0]
 
-def convertPILtocv2(im):
-    cv2_im = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
-    return cv2_im
+  for count, colour in pixels:
+      if count > most_frequent_pixel[0]:
+          most_frequent_pixel = (count, colour)
 
-def convertcv2toPIL(cv2_im):
-    (height, width, colors) = cv2_im.shape
-    if colors == 4:
-        pil_im = Image.fromarray(cv2.cvtColor(cv2_im, cv2.COLOR_BGRA2RGBA))
-    else:
-        pil_im = Image.fromarray(cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB))
-    return pil_im
-
-def make_background_magenta(PILforeground_source, PILbackground_source, erode_width):
-    # take a background source that has a pure magenta background, and replace the background on the foreground source with it.
-    # this only works well if the background source was used with controlnet to generate the foreground source.
-    # erode_width is how much the background should contract before being applied
-    foreground_source = convertPILtocv2(PILforeground_source)
-    background_source = convertPILtocv2(PILbackground_source)
-    (height, width, colors) = foreground_source.shape
-    print(foreground_source.shape)
-    background_source = cv2.resize(background_source,(width, height), interpolation=cv2.INTER_LINEAR)
-    mask =  np.all(background_source == [255,0,255], axis=-1)
-    print(mask.dtype)
-    if erode_width>0:
-        kernel = np.ones((erode_width, erode_width), np.uint8)
-        mask = cv2.erode(mask.astype(np.uint8),kernel)
-    else:
-        kernel = np.ones((-erode_width, -erode_width), np.uint8)
-        mask = cv2.dilate(mask.astype(np.uint8),kernel)
-    foreground_source[mask.astype(bool)] = [255,0,255]
-
-    # opacity = np.where(foreground_source == [255,0,255])
-    # print(opacity[0].shape)
-    transparent_array = np.zeros((foreground_source.shape[0],foreground_source.shape[1],4),np.uint8)
-    transparent_array[:,:,:3]=foreground_source
-    transparent_array[:,:,3]= np.where(foreground_source == [255,0,255], [0], [255])[:,:,0]
-
-    print(transparent_array.shape)
-
-    output = convertcv2toPIL(transparent_array)
-    return output
+  for x in range(width):
+      for y in range(height):
+          pixel = img.getpixel((x, y))
+          if abs(pixel[0] - most_frequent_pixel[1][0]) < 10 and abs(pixel[1] - most_frequent_pixel[1][1]) < 10 and abs(pixel[2] - most_frequent_pixel[1][2]) < 10:
+              img.putpixel((x, y), (255, 255, 255, 0))
+  
+  img = crop(img)   
+  img = cleanImage(img)
+  return img
--- predict.py
+++ predict.py
@@ -2,8 +2,8 @@
 # https://github.com/replicate/cog/blob/main/docs/python.md
 
 from cog import BasePredictor, BaseModel, File, Input, Path
-from base import init_model, make_background_magenta, load_image_generalised, inference, inference_w_gpt, inference_with_edge_guidance, init_hed_controlnet
-from postprocess import cut, cutv2, cut_magenta, splitHeightTo2, splitImageTo9, img2b4
+from base import load_image_generalised, inference, init_hed_controlnet
+from postprocess import splitHeightTo2, splitImageTo9, img2b4, cleanImage_wrapper
 from PIL import Image
 
 
@@ -58,13 +58,9 @@ class Predictor(BasePredictor):
         guidance_scale: float = Input(description="Prompt Guidance strength/Classifier Free Generation strength of Stable Diffusion", default=7.5),
         split : str = Input(description="Decide which split needs to happen", default="none"),
         req_type: str = Input(description="Describes whether the request is for an object asset or a tile", default="asset"),
-        negative_prompt: str = Input(description="Negative_Prompt", default="base, ground, terrain, child's drawing, sillhouette, dark, shadowed, green blob, cast shadow on the ground, background pattern"),
+        negative_prompt: str = Input(description="Negative_Prompt", default=""),
         num_inference_steps: int = Input(description="Number of denoising steps", default = 20),
-        # cut_inner_tol:int = Input(description="Inner tolerance in `cutv2` strongest component PNG masking ", default = 7),
-        outer_tol:int = Input(description="Outer tolerance in `cutv2` strongest component PNG masking ", default = 80),
-        # cut_radius:int = Input(description="Radius in `cutv2` strongest component PNG masking ", default = 70),
         sd_seed:int = Input(description="Seed for SD generations for getting deterministic outputs", default = None),
-        erode_width:int = Input(description="Canny BG Removal argument", default = 5),
         width:int = Input(description="Width for returning output image", default = None),
         height:int = Input(description="Height for returning output image", default = None)
     ) -> Any:
@@ -99,7 +95,7 @@ class Predictor(BasePredictor):
                 for gen_image in images:
                     # images_.append(cutv2(gen_image, init_img, outer_tolerance = cut_outer_tol, inner_tolerance = cut_inner_tol, radius = cut_radius))
                     # images_.append(cut_magenta(gen_image, outer_tol))
-                    images_.append(make_background_magenta(gen_image, init_img , erode_width))
+                    images_.append(cleanImage_wrapper(gen_image))
             else:
                 for image in images:
                     images_.append(image)
